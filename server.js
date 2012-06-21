dbe61d0181887d7639ead294e8544a5d5941957c
Wouter Scherphof <wouter.scherphof@gmail.com>
2012-06-21 12:19:36 +0200

    A preliminary page with a form for posting clues

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 var http = require('http');
+var util = require('util');
+var express = require('express');
 
-var server = http.createServer(function (req, res) {
-  console.log(req);
-  res.close();
+var app = express.createServer();
+
+app.configure(function () {
+  app.use(express.bodyParser());
 });
 
-server.listen(process.env.PORT, '0.0.0.0');
+var clues = [];
+var clueFields = [];
+app.get('/clues', function (req, res) {
+  res.render('clues.jade', {layout: false});
+});
+
+app.post('/clues', function (req, res) {
+  var lines = req.body.clues.split('\r\n');
+  if (req.body.purge) clues = clueFields = [];
+  if (req.body.fieldNames) {
+    var firstLine = lines.shift();
+    if (clueFields.length === 0) clueFields = firstLine.split(',');
+  }
+  lines.forEach(function (line) {
+    if (line === '') return;
+    var clue = {};
+    var values = line.split(',');
+    values.forEach(function (value, i) {
+      var name = clueFields[i] || i;
+      clue[name] = value;
+    });
+    clues.push(clue);
+  });
+  res.end(util.inspect(clues));
+});
+
+
+app.listen(3000);
